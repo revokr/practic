3f23747ffd58c2fe127d2852c693e1c9542b2443
revokr <101871749+revokr@users.noreply.github.com>
2024-01-31 13:23:52 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from ui.consola import consola
+from teste.teste import teste
+
+Consola = consola("C:\\Users\\rafael\\PycharmProjects\\practic\\examene.txt")
+
+Teste = teste()
+Teste.ruleaza()
+
+Consola.run()
